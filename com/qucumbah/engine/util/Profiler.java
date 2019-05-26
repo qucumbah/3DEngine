19c456f2b269467a54479eb254862eec3551eb41
qucumbah <sc.buher@yandex.ru>
2019-05-26 17:15:44 +0300

    debug code for profiler

--- com/qucumbah/engine/util/Profiler.java
+++ com/qucumbah/engine/util/Profiler.java
@@ -55,31 +55,63 @@ public class Profiler {
   }
 
   public void logTotalTime(int mode) {
-    System.out.println("TIMESTAMPS:");
+    System.out.println("Average time ("+numberOfRecords+" calls):");
     summedTimestapms.forEach((key,value)->{
-      System.out.println(key+":"+value/mode);
+      System.out.println(key+":"+value/numberOfRecords/mode);
     });
   }
 
   //debug
+  public static double millionAdditions() {
+    double result = 0;
+    for (int i = 0;i<1e6;i++) {
+      result+=Math.random();
+    }
+    return result;
+  }
+
+  public static double millionMultiplications() {
+    double result = 1;
+    for (int i = 0;i<1e6;i++) {
+      result*=Math.random();
+    }
+    return result;
+  }
+
+  public static double millionP3DOperations() {
+    for (int i = 0;i<1e6;i++) {
+      javafx.geometry.Point3D p1 = new javafx.geometry.Point3D(Math.random(),Math.random(),Math.random());
+      javafx.geometry.Point3D p2 = new javafx.geometry.Point3D(Math.random(),Math.random(),Math.random());
+      p1.crossProduct(p2);
+    }
+    return 0;
+  }
+
   public static void main(String[] args) {
     Profiler p = new Profiler();
 
     p.start();
-    int k = 0;
-    for (int i = 0;i<1e6;i++) {
-      k++;
-    }
-    p.add("firstDone");
-    for (int i = 0;i<1e6;i++) {
-      k++;
-    }
-    p.add("secondDone");
-    for (int i = 0;i<1e7;i++) {
-      k++;
-    }
+    millionAdditions();
+    p.add("additions");
+    millionMultiplications();
+    p.add("multiplications");
+    millionP3DOperations();
+    p.add("Point3D operations");
     p.stop();
 
-    p.logTotalTime(MILISECONDS);
+    p.logTotalTime(100);
+
+    for (int i = 0;i<5;i++) {
+      p.start();
+      millionAdditions();
+      p.add("additions");
+      millionMultiplications();
+      p.add("multiplications");
+      System.out.println(millionP3DOperations());
+      p.add("Point3D operations");
+      p.stop();
+    }
+
+    p.logTotalTime(100);
   }
 }
